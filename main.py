13fef7ccd25d92fa2d17e9969871b58affbf4735
renald <renald.a9@gmail,com>
2022-09-26 21:49:41 +0300

    доп.меню. Удаление лишних функций

--- main.py
+++ main.py
@@ -22,39 +22,58 @@ bot = telebot.TeleBot('5654043644:AAHJ-rFpzwvzkjehr2tWg4FzksoG81WmGiI')
 def start (message):
     send_mess = f"Привет, {message.from_user.first_name}"
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
-    btn1 = types.KeyboardButton('биток')
+    btn1 = types.KeyboardButton('Bitcoin')
     btn2 = types.KeyboardButton('Ethereum')
     markup.add(btn1, btn2)
     bot.send_message(message.chat.id, send_mess, parse_mode='html', reply_markup=markup)   
 
-@bot.message_handler(commands=['btc'])
-def btc (message):
-    markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("Посетить веб сайт/курс Bitcoin", url="https://coinmarketcap.com/currencies/bitcoin/"))
-    bot.send_message(message.chat.id, 'Курс Bitcoin', reply_markup=markup)
-
-@bot.message_handler(commands=['eth'])
-def eth (message):
-    markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("Посетить веб сайт/курс Ethereum", url="https://coinmarketcap.com/currencies/ethereum/"))
-    bot.send_message(message.chat.id, 'Курс Ethereum', reply_markup=markup)
+@bot.message_handler(commands=['menu'])
+def menu (message):
+    send_mess = 'Меню'
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
+    btn1 = types.KeyboardButton('Bitcoin')
+    btn2 = types.KeyboardButton('Ethereum')
+    markup.add(btn1, btn2)
+    bot.send_message(message.chat.id, send_mess, parse_mode='html', reply_markup=markup)   
 
 @bot.message_handler(content_types='text')
 def mess(message):
-    get_message_bot = message.text.strip().lower()
+    get_message_bot = message.text.strip()
 
-    if get_message_bot == 'биток':
+    if get_message_bot == 'Bitcoin':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+        btn1 = types.KeyboardButton('Курс Bitcon')
+        btn2 = types.KeyboardButton('График')
+        btn3 = types.KeyboardButton('Назад')
+        markup.add(btn1, btn2, btn3)
+        bot.send_message(message.chat.id, 'Bitcoin', reply_markup=markup)
+
+    elif get_message_bot == 'Ethereum':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+        btn1 = types.KeyboardButton('Курс Ethereum')
+        btn2 = types.KeyboardButton('График')
+        btn3 = types.KeyboardButton('Назад')
+        markup.add(btn1, btn2, btn3)
+        bot.send_message(message.chat.id, 'Ethereum', reply_markup=markup)
+    
+
+    elif get_message_bot == 'Курс Bitcon':
         markup = types.InlineKeyboardMarkup()
         markup.add(types.InlineKeyboardButton("Посетить веб сайт/курс Bitcoin", url="https://coinmarketcap.com/currencies/bitcoin/"))
         bot.send_message(message.chat.id, 'Курс Bitcoin', reply_markup=markup)
-    
-    elif get_message_bot == 'Ethereum':
+
+    elif get_message_bot == 'Курс Ethereum':
         markup = types.InlineKeyboardMarkup()
         markup.add(types.InlineKeyboardButton("Посетить веб сайт/курс Ethereum", url="https://coinmarketcap.com/currencies/ethereum/"))
-        bot.send_message(message.chat.id, 'Курс Ethereum', reply_markup=markup)   
+        bot.send_message(message.chat.id, 'Курс Ethereum', reply_markup=markup)
+
+    elif get_message_bot == 'Назад':
+        menu (message)
 
     else:
           bot.send_message(message.chat.id, 'Ошибка') 
+
+
 
 
 
