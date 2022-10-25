1fc6058ac19dcb2f18df771a82a1eb04040a7d8c
renald <renald.a9@gmail,com>
2022-10-25 16:11:43 +0300

    price change

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import telebot
 from telebot import types
 
+
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
@@ -14,14 +15,22 @@ cmc = CoinMarketCapAPI(api_key)
 r = cmc.cryptocurrency_info(symbol='BTC')
 data_quote = cmc.cryptocurrency_quotes_latest(symbol='BTC', convert='USD')
 
+# test
 x = data_quote
 y = x.data['BTC']['quote']['USD']['price']
 round_price = round(y, 2)
-
 print(round_price)
+# процентное изменение цены
+x1 = cmc.cryptocurrency_listings_latest()
+y1 = x1.data[0]['quote']['USD']['percent_change_1h'] 
+print(y1, '%')
+
 
 bot = telebot.TeleBot('5654043644:AAHJ-rFpzwvzkjehr2tWg4FzksoG81WmGiI')
 
+# @bot.message_handler()
+# def get_user_text (message):
+#     bot.send_message(message.chat.id, message, parse_mode='html')
 
 @bot.message_handler(commands=['start'])
 def start (message):
@@ -47,83 +56,97 @@ def menu (message):
     markup.add(btn1, btn2, btn3, btn4, btn5)
     bot.send_message(message.chat.id, send_mess, parse_mode='html', reply_markup=markup)   
 
+
+# @bot.message_handler(content_types=['text'])
+# def all_messages(message):
+#     bot.send_message(chat_id=226048369, text='тестовое сообщение')
+
 @bot.message_handler(content_types='text')
 def mess(message):
     get_message_bot = message.text.strip()
 
-    if get_message_bot == 'Bitcoin':
+    if get_message_bot in ['Bitcoin','Ethereum','Tether', 'USD Coin', 'BNB'] :
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-        btn1 = types.KeyboardButton('Курс Bitcon')
-        btn2 = types.KeyboardButton('График')
+        btn1 = types.KeyboardButton(f'{get_message_bot} Price')
+        btn2 = types.KeyboardButton(f' {get_message_bot} Price change')
         btn3 = types.KeyboardButton('Назад')
         markup.add(btn1, btn2, btn3)
-        bot.send_message(message.chat.id, text="Bitcon", reply_markup=markup)
-
-    elif get_message_bot == 'Ethereum':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-        btn1 = types.KeyboardButton('Курс Ethereum')
-        btn2 = types.KeyboardButton('График')
-        btn3 = types.KeyboardButton('Назад')
-        markup.add(btn1, btn2, btn3)
-        bot.send_message(message.chat.id, 'Ethereum', reply_markup=markup)
-
-    elif get_message_bot == 'Tether':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-        btn1 = types.KeyboardButton('Курс Tether')
-        btn2 = types.KeyboardButton('График')
-        btn3 = types.KeyboardButton('Назад')
-        markup.add(btn1, btn2, btn3)
-        bot.send_message(message.chat.id, 'Tether', reply_markup=markup)
-    
-    elif get_message_bot == 'USD Coin':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-        btn1 = types.KeyboardButton('Курс USD Coin')
-        btn2 = types.KeyboardButton('График')
-        btn3 = types.KeyboardButton('Назад')
-        markup.add(btn1, btn2, btn3)
-        bot.send_message(message.chat.id, 'USD Coin', reply_markup=markup)
-
-    elif get_message_bot == 'BNB':
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-        btn1 = types.KeyboardButton('Курс BNB')
-        btn2 = types.KeyboardButton('График')
-        btn3 = types.KeyboardButton('Назад')
-        markup.add(btn1, btn2, btn3)
-        bot.send_message(message.chat.id, 'BNB', reply_markup=markup)
-
-    elif get_message_bot == 'Курс Bitcon':
+        bot.send_message(message.chat.id, text=f"{get_message_bot}", reply_markup=markup)
+   
+    elif get_message_bot == 'Bitcoin Price':
         data_quote = cmc.cryptocurrency_quotes_latest(symbol='BTC', convert='USD')
-        # x = data_quote
         price_btc = data_quote.data['BTC']['quote']['USD']['price']
         round_price = round(price_btc, 2)
-        # markup = types.InlineKeyboardMarkup()
-        # markup.add(types.InlineKeyboardButton("Посетить веб сайт/курс Bitcoin", url="https://coinmarketcap.com/currencies/bitcoin/"))
-        bot.send_message(message.chat.id, text=round_price)
+        bot.send_message(message.chat.id, f'{round_price} $')
         
-    elif get_message_bot == 'Курс Ethereum':
+    elif get_message_bot == 'Ethereum Price':
         data_quote = cmc.cryptocurrency_quotes_latest(symbol='ETH', convert='USD')
         price_eth = data_quote.data['ETH']['quote']['USD']['price']
         round_price = round(price_eth, 2)
-        bot.send_message(message.chat.id, text=round_price)
+        bot.send_message(message.chat.id, f'{round_price} $')
 
-    elif get_message_bot == 'Курс Tether':
+    elif get_message_bot == 'Tether Price':
         data_quote = cmc.cryptocurrency_quotes_latest(symbol='USDT', convert='USD')
         price_usdt = data_quote.data['USDT']['quote']['USD']['price']
         round_price = round(price_usdt, 2)
-        bot.send_message(message.chat.id, text=round_price)
+        bot.send_message(message.chat.id, f'{round_price} $')
 
-    elif get_message_bot == 'Курс USD Coin':
+    elif get_message_bot == 'USD Coin Price':
         data_quote = cmc.cryptocurrency_quotes_latest(symbol='USDC', convert='USD')
         price_usdc = data_quote.data['USDC']['quote']['USD']['price']
         round_price = round(price_usdc, 2)
-        bot.send_message(message.chat.id, text=round_price)
+        bot.send_message(message.chat.id, f'{round_price} $')
 
-    elif get_message_bot == 'Курс BNB':
+    elif get_message_bot == 'BNB Price':
         data_quote = cmc.cryptocurrency_quotes_latest(symbol='BNB', convert='USD')
         price_bnb = data_quote.data['BNB']['quote']['USD']['price']
         round_price = round(price_bnb, 2)
-        bot.send_message(message.chat.id, text=round_price)
+        bot.send_message(message.chat.id, f'{round_price} $')
 
+    elif get_message_bot == 'Bitcoin Price change':
+        lastest = cmc.cryptocurrency_listings_latest()
+        lastest_bnb1 = round(lastest.data[0]['quote']['USD']['percent_change_1h'],3)
+        lastest_bnb2 = round(lastest.data[0]['quote']['USD']['percent_change_24h'], 3)
+        lastest_bnb3 = round(lastest.data[0]['quote']['USD']['percent_change_7d'], 3)
+        bot.send_message(message.chat.id, f'За 1 час: {lastest_bnb1} %\n'
+                                        f'За 24 часа: {lastest_bnb2} %\n'
+                                        f'За 7 дней: {lastest_bnb3} %\n') 
+                                    
+    elif get_message_bot == 'Ethereum Price change':
+        lastest = cmc.cryptocurrency_listings_latest()
+        lastest_bnb1 = round(lastest.data[1]['quote']['USD']['percent_change_1h'],3)
+        lastest_bnb2 = round(lastest.data[1]['quote']['USD']['percent_change_24h'], 3)
+        lastest_bnb3 = round(lastest.data[1]['quote']['USD']['percent_change_7d'], 3)
+        bot.send_message(message.chat.id, f'За 1 час: {lastest_bnb1} %\n'
+                                        f'За 24 часа: {lastest_bnb2} %\n'
+                                        f'За 7 дней: {lastest_bnb3} %\n') 
+        
+    elif get_message_bot == 'Tether Price change':
+        lastest = cmc.cryptocurrency_listings_latest()
+        lastest_bnb1 = round(lastest.data[2]['quote']['USD']['percent_change_1h'],3)
+        lastest_bnb2 = round(lastest.data[2]['quote']['USD']['percent_change_24h'], 3)
+        lastest_bnb3 = round(lastest.data[2]['quote']['USD']['percent_change_7d'], 3)
+        bot.send_message(message.chat.id, f'За 1 час: {lastest_bnb1} %\n'
+                                        f'За 24 часа: {lastest_bnb2} %\n'
+                                        f'За 7 дней: {lastest_bnb3} %\n') 
+
+    elif get_message_bot == 'USD Coin Price change':
+        lastest = cmc.cryptocurrency_listings_latest()
+        lastest_bnb1 = round(lastest.data[3]['quote']['USD']['percent_change_1h'],3)
+        lastest_bnb2 = round(lastest.data[3]['quote']['USD']['percent_change_24h'], 3)
+        lastest_bnb3 = round(lastest.data[3]['quote']['USD']['percent_change_7d'], 3)
+        bot.send_message(message.chat.id, f'За 1 час: {lastest_bnb1} %\n'
+                                        f'За 24 часа: {lastest_bnb2} %\n'
+                                        f'За 7 дней: {lastest_bnb3} %\n') 
+    
+    elif get_message_bot == 'BNB Price change':
+        lastest = cmc.cryptocurrency_listings_latest()
+        lastest_bnb1 = round(lastest.data[4]['quote']['USD']['percent_change_1h'],3)
+        lastest_bnb2 = round(lastest.data[4]['quote']['USD']['percent_change_24h'], 3)
+        lastest_bnb3 = round(lastest.data[4]['quote']['USD']['percent_change_7d'], 3)
+        bot.send_message(message.chat.id, f'За 1 час: {lastest_bnb1} %\n'
+                                        f'За 24 часа: {lastest_bnb2} %\n'
+                                        f'За 7 дней: {lastest_bnb3} %\n') 
 
     elif get_message_bot == 'Назад':
         menu (message)
@@ -131,19 +154,28 @@ def mess(message):
     else:
           bot.send_message(message.chat.id, 'Ошибка') 
 
-@bot.callback_query_handler(func=lambda call: True)
-def query_handler(call):
-
-    bot.answer_callback_query(callback_query_id=call.id)
-    answer = ''
-    if call.data == 'Курс Bitcon':
-        answer = data_quote.data['BTC']['quote']['USD']['price']
-    else:
-        answer = 'ошибка'
-
-    bot.send_message(call.message.chat.id, answer)
 
 
+
+@bot.message_handler(content_types='text')
+def send_message(bot, job):
+    text = 'Это тестовое сообщение'
+    bot.sendMessage(chat_id=user[357330578], text=text)
+
+
+# @bot.callback_query_handler(func=lambda call: True)
+# def query_handler(call):
+
+#     bot.answer_callback_query(callback_query_id=call.id)
+#     answer = ''
+#     if call.data == 'Курс Bitcon':
+#         answer = data_quote.data['BTC']['quote']['USD']['price']
+#     else:
+#         answer = 'ошибка'
+
+#     bot.send_message(call.message.chat.id, answer)
+
+    
 
 
 bot.polling(none_stop=True)
